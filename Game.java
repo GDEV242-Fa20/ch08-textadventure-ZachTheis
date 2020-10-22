866f5b580351fe237b1652530ee969b235667858
ZachTheis <zach.theis+zacharywstheis@gmail.com>
2020-10-21 20:08:00 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -11,8 +11,8 @@
  *  rooms, creates the parser and starts the game.  It also evaluates and
  *  executes the commands that the parser returns.
  * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author  Zach Theis
+ * @version 2020.10.25
  */
 
 public class Game 
